960c71ea67bb11a18569b7930fe7d0fbdd84df78
AhmedShimmas <mushayyy007@gmail.com>
2026-07-03 16:21:53 +0500

    added ahmad-dev dir, and a problems dir that will hold all our problems in a single problem.py file

---
+++ problems/problems.py
@@ -0,0 +1,8 @@
+problem_01 = """
+A string such that it has duplicate characters in it. The string can be of any length and can contain any characters.
+Your solution should eliminate all the duplicate characters from the string and return a new string with only unique characters.
+
+eg. 
+Input: "programming"
+Output: "progamin"
+"""
